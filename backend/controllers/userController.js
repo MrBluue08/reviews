59b7d33323c91c58c27cc7b693fceeb1ea526334
MrBluue08 <joel.gurrera@inslapineda.cat>
2024-04-25 13:27:49 +0200

    no c

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const user = require('../models/user');
+const bcrypt = require('bcrypt');
 
 router.post('/registerUser',async (req, res) => {
     try {
         console.log(req.body);
+        const user = new User({
+            email: req.body.email,
+            username: req.body.userName,
+
+        })
         res.status(200).send("User registered successfully");
     } catch (err) {
         console.error(err);
