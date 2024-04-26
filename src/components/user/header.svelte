9014a08b8c54238438213adc5e5df398b4c5f709
MrBluue08 <joel.gurrera@inslapineda.cat>
2024-04-26 13:06:46 +0200

    estamos con lo del registro

--- src/components/user/header.svelte
+++ src/components/user/header.svelte
@@ -1,12 +1,45 @@
 <script>
+    let email,userName,password;
 
 </script>
 
+
+<div class="navbar">
+<!-- Logo -->
+    <a href="#" class="logo">Your Logo</a>
+
+    <!-- Navigation links -->
+    <a href="#">My Profile</a>
+    <a href="#">Sign Up</a>
+    <a href="#">Sign In</a>
+</div>
+
+<div class="popUp">
+    <form>
+        <label for="mail">Email:</label>
+        <input type="email" name="mail" bind:value={email} required>
+        <label for="userName">User name:</label>
+        <input type="text" name="userName" bind:value={userName} required>
+        <label for="password">Password:</label>
+        <input type="password" name="password" bind:value={password} required>
+        <button type="submit">Register</button>
+    </form>
+</div>
+
 <style>
+    .popUp{
+        margin: 0;
+        margin-top: 45px;
+        margin-left: 40%;
+        margin-right: 40%;
+        position: absolute;
+        z-index: 9;
+    }
     /* Basic styling for the navbar */
     .navbar {
         background-color: #333;
         overflow: hidden;
+        overflow-y: auto;
         position: fixed;
         top: 0;
         width: 100%;
@@ -37,13 +70,3 @@
         color: black;
     }
 </style>
-
-<div class="navbar">
-<!-- Logo -->
-    <a href="#" class="logo">Your Logo</a>
-
-    <!-- Navigation links -->
-    <a href="#">My Profile</a>
-    <a href="#">Sign Up</a>
-    <a href="#">Sign In</a>
-</div>
